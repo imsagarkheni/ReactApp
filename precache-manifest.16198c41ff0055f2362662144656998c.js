139e59c5a2e63a944394924ca4527cc03bd66f2f
Sagar Kheni <sagarkheni34@gmail.com>
2022-07-15 14:14:17 +0530

    Updates

---
+++ precache-manifest.16198c41ff0055f2362662144656998c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8137675bb1930dc3a0bf",
+    "url": "/ReactApp/static/css/main.8631124d.chunk.css"
+  },
+  {
+    "revision": "8137675bb1930dc3a0bf",
+    "url": "/ReactApp/static/js/main.8137675b.chunk.js"
+  },
+  {
+    "revision": "783603c31ac26cbc2d6a",
+    "url": "/ReactApp/static/css/1.15c2e087.chunk.css"
+  },
+  {
+    "revision": "783603c31ac26cbc2d6a",
+    "url": "/ReactApp/static/js/1.783603c3.chunk.js"
+  },
+  {
+    "revision": "2e2b30ee4b57700ce9f2",
+    "url": "/ReactApp/static/js/runtime~main.2e2b30ee.js"
+  },
+  {
+    "revision": "e980c1173022b1e27482699556c1afce",
+    "url": "/ReactApp/static/media/image2.e980c117.gif"
+  },
+  {
+    "revision": "3cdf50bf2536bb0a51cb28e1a291e670",
+    "url": "/ReactApp/static/media/err.3cdf50bf.gif"
+  },
+  {
+    "revision": "d53099bec61c6c2e8556b3743ad18ee3",
+    "url": "/ReactApp/index.html"
+  }
+];
